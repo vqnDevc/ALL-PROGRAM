e71665c680fcbf05f0b1885b9b98e003f4aa3471
vqnDevc <phanquyen312003@gmail.com>
2023-04-21 20:13:15 +0700

    test java

--- Java/TEST_TWO/src/App.java
+++ Java/TEST_TWO/src/App.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        int age = input.nextInt();
+        double age = input.nextDouble();
 
         System.out.println("Hello, World! " + age);
         input.close();
