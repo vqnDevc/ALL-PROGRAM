53e5726b0749034d60a9427b2e52e24160a14043
vqnDevc <phanquyen312003@gmail.com>
2023-04-23 00:25:58 +0700

    test java

--- Java/TEST_TWO/src/App.java
+++ Java/TEST_TWO/src/App.java
@@ -3,10 +3,20 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        double age = input.nextDouble();
 
-        System.out.println("Hello, World! " + age);
+        int[] numbers = new int[4];
+
+        for(int i = 0; i < numbers.length; i++){
+            numbers[i] = input.nextInt();
+        }
+        // numbers[0] = 1;
+        // numbers[1] = 2;
+        // numbers[2] = 3;
+        // numbers[3] = 4;
+
+        for(int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
         input.close();
-
     }
 }
