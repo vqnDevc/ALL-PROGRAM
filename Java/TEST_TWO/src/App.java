31d293d3e4944d5c35df399a60819a911cc27214
vqnDevc <phanquyen312003@gmail.com>
2023-04-24 23:19:04 +0700

    upload file java

--- Java/TEST_TWO/src/App.java
+++ Java/TEST_TWO/src/App.java
@@ -1,22 +1,37 @@
 import java.util.Scanner;
 
 public class App {
+    void printNames(String[] names){
+        for(String name : names){
+            System.out.println(name);
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
 
-        int[] numbers = new int[4];
+        String[] names = new String[4];
 
-        for(int i = 0; i < numbers.length; i++){
-            numbers[i] = input.nextInt();
+        for(int i = 0; i < names.length; i++){
+            names[i] = input.nextLine();
         }
+         
+        App obj = new App();
+        obj.printNames(names);
+
+        // charAt() truy cap ky tu trong chuoi String tai vi tri index
+        // concat() noi 2 chuoi voi nhau
+        // equals() so sanh 2 chuoi
+        // string.replace(char oldValue, char newValue) : thay doi xau con ben trong cua 1 xau
+        // VD: s1 = Full House; s1.replace("Full", "Null"); s1 = Null House;
+        // string.toLowerCase() chuyen string ve chu thuong
+        // string.toUpperCase() chuyen string ve chu Hoa
+
         // numbers[0] = 1;
         // numbers[1] = 2;
         // numbers[2] = 3;
         // numbers[3] = 4;
 
-        for(int number : numbers){
-            System.out.println(number);
-        }
         input.close();
     }
 }
