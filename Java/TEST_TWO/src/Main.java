3d500160388c22641126ffc92cc21d14d362a316
vqnDevc <phanquyen312003@gmail.com>
2023-04-25 21:58:29 +0700

    upload files oop java

--- Java/TEST_TWO/src/Main.java
+++ Java/TEST_TWO/src/Main.java
@@ -5,12 +5,20 @@ class Student {
     
     int score;
     char grade;
-    
+    private String name;
     // assign value to score
     Student(int score) {
         this.score = score;
     }
+
+    public void setName(String name){
+        this.name = name;
+    }
     
+    public String getName(){
+        return this.name;
+    }
+
     void assignGrade() {
         
         // assign grade based on the value
@@ -31,16 +39,22 @@ class Main {
         
         // take the score input
         Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter student's name: ");
+        String name = input.nextLine();
+
+        System.out.println("Enter " + name + " score: ");
         int score = input.nextInt();
         
         // create object
         Student student = new Student(score);
-        
+        student.setName(name);
         // call the assignGrade() method
         student.assignGrade();
         
         // print the grade
-        System.out.println(student.grade);
+        System.out.println("Student: " + student.getName());
+        System.out.println("Grade: " + student.grade);
         input.close();
     }
 }
