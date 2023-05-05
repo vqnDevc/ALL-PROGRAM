2ade794725701e8db133122a74230e1e65f646cf
vqnDevc <phanquyen312003@gmail.com>
2023-05-05 20:45:36 +0700

    update game caro

--- Java/TEST_TWO/src/caro.java
+++ Java/TEST_TWO/src/caro.java
@@ -1,154 +1,105 @@
 import java.util.Scanner;
 
 class Board {
-    
     int count = 0;
-    String board[] = { " ", " ", " ", " ", " ", " ", " ", " ", " " };
- 
-    void printBoard() {
- 
+    String board[] = {" "," "," "," "," "," "," "," "," "};
+
+    void printBoard(){
         System.out.println(" " + board[0] + " | " + board[1] + " | " + board[2]);
         System.out.println("-----------");
         System.out.println(" " + board[3] + " | " + board[4] + " | " + board[5]);
         System.out.println("-----------");
         System.out.println(" " + board[6] + " | " + board[7] + " | " + board[8]);
- 
     }
 
-    boolean updateBoard(int position, String type) {
- 
-        // if a player selects position n, n-1 index should be updated
-        // if the position is not already filled, update the board
-        if (board[position - 1] == " ") {
+    boolean updateBoard(int position, String type){
+        if(board[position - 1] == " "){
             board[position - 1] = type;
- 
-            // increase the value of count by 1
             count++;
             return true;
-        }
- 
-        // if the position is already filled, ask user to select another position
-        else {
+        }else{
             System.out.println("Position already selected. Select another position.");
             return false;
- 
         }
-    }
- 
-    // If all fields are selected and there is no winner, it's draw
-    // Returning true if it's draw
-    boolean checkDraw() {
- 
-        // check if count == 9
-        if (count == 9) {
-            return true;
-        } 
-        else {
-            return false;
-        }
-    }
- 
-    // if three symbols appears in a row, returning True
-    boolean checkWinner(String type) {
-        if ((board[0] == type && board[1] == type && board[2] == type)
-                || (board[3] == type && board[4] == type && board[5] == type)
-                || (board[6] == type && board[7] == type && board[8] == type)
-                || (board[0] == type && board[3] == type && board[6] == type)
-                || (board[1] == type && board[4] == type && board[7] == type)
-                || (board[2] == type && board[5] == type && board[8] == type)
-                || (board[0] == type && board[4] == type && board[8] == type)
-                || (board[2] == type && board[4] == type && board[6] == type)) {
-            return true;
-        }
- 
-        else {
-            return false;
-        }
- 
     }
 
+    boolean checkDraw(){
+        return (count == 9);
+    }
+
+    boolean checkWin(String type){
+        return  (board[0] == type && board[1] == type && board[2] == type) ||
+                (board[3] == type && board[4] == type && board[5] == type) ||
+                (board[6] == type && board[7] == type && board[8] == type) ||
+                (board[0] == type && board[3] == type && board[6] == type) ||
+                (board[1] == type && board[4] == type && board[7] == type) ||
+                (board[2] == type && board[5] == type && board[8] == type) ||
+                (board[0] == type && board[4] == type && board[8] == type) ||
+                (board[2] == type && board[4] == type && board[6] == type);
+    }
 }
 
 class Player {
- 
     String name;
     String type;
- 
-    Player(String type) {
+
+    Player(String type){
         this.type = type;
         Scanner input = new Scanner(System.in);
-        if (type == "X") {
+        if(type == "X"){
             System.out.println("Player selecting X, enter your name: ");
-            name = input.nextLine();
-        } 
-        else {
+            this.name = input.nextLine();
+        }else{
             System.out.println("Player selecting O, enter your name: ");
-            name = input.nextLine();
+            this.name = input.nextLine();
         }
     }
- 
 }
 
 class Gameplay {
- 
     Board board = new Board();
     Player player1 = new Player("X");
     Player player2 = new Player("O");
- 
-    Player currentPlayer = player1;
- 
-    void play() {
-        // using an infinite loop to run the game
-        // we will later add conditions to terminate the loop
-        String message = "enter the position (1 - 9):";
-        while (true) {
- 
-            System.out.println(currentPlayer.name + " " + message);
+
+    Player cursor = player1;
+
+    void play(){
+
+        String message = " enter the position (1 - 9)";
+        while(true){
+            System.out.print(cursor.name + message + ": ");
             Scanner input = new Scanner(System.in);
             int position = input.nextInt();
- 
-            // the updateBoard() method return true if
-            // the user selected position is not already filled
-            if (board.updateBoard(position, currentPlayer.type)) {
- 
-                board.printBoard();
- 
-                // checking winner each time after updating the board
-                if (board.checkWinner(currentPlayer.type)) {
-                    System.out.println(currentPlayer.name + " wins!");
-                    break;
-                }
- 
-                // checking draw each time after updating the board
-                else if (board.checkDraw()) {
-                    System.out.println("Game is a draw!");
-                    break;
-                }
- 
-                // changing current player when board is updated
-                else {
-                    if (currentPlayer == player1) {
-                        currentPlayer = player2;
-                    }
- 
-                    else {
-                        currentPlayer = player1;
-                    }
- 
-                }
+            while(position < 1 || position > 9){
+                System.out.println("Invalid position, retype!");
+                System.out.print(cursor.name + message + ": ");
+                position = input.nextInt();
             }
- 
+
+            if(board.updateBoard(position, cursor.type)){
+                board.printBoard();
+
+                if(board.checkWin(cursor.type)){
+                    System.out.println(cursor.name + " wins!");
+                    break;
+                }else if (board.checkDraw()){
+                    System.out.println("Game is a Draw!");
+                    break;
+                }else{
+                    if(cursor == player1){
+                        cursor = player2;
+                    }else{
+                        cursor = player1;
+                    }
+                } 
+            }
         }
- 
     }
- 
 }
-
-
 
 public class caro {
     public static void main(String[] args) {
-        Gameplay game = new Gameplay();
-        game.play();
+        Gameplay newgame = new Gameplay();
+        newgame.play();
     }
 }
