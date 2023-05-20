c5242818ce8ccfde9ef8edf78575544c0f64e771
vqnDevc <phanquyen312003@gmail.com>
2023-05-21 00:50:40 +0700

    priority_queue

--- LinkedList/Heap/priority_queue.cpp
+++ LinkedList/Heap/priority_queue.cpp
@@ -4,11 +4,9 @@ using namespace std;
 int main() {
     // Note: C++ priority_queue implements a max heap by default
     
-    vector<int> nums = {67, 341, 234, -67, 12, -976};
-    priority_queue<int> heap(nums.begin(), nums.end());
+    vector<char> nums = {'C','A','E','H','D'};
+    priority_queue<char> heap(nums.begin(), nums.end());
     
-    heap.push(7451);
-    heap.push(-5352);
     
     // The numbers will be printed in sorted order
     while (!heap.empty()) {
